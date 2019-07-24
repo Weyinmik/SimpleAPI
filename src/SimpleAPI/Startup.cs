33697c881280f837654c2318f7ba9f1caefbe4ed
Esther Isack <esther.isack@avanade.com>
2019-07-24 18:55:12 +0200

    Removed https redirection

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -40,8 +40,6 @@ namespace SimpleAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
